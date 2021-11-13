f01a98435237cc3840b8028573f91f5c07135d37
Jason Hazel <jhazel@matrixmediaservices.com>
2021-11-12 20:32:20 -0500

    update build script for multiple builds in same day

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
+START=`date +%s`
 BUILD=`date +"%Y.%m.%d"`
-
-rm -rf usr && \
-npx vue-cli-service build --dest usr/local/emhttp/plugins/undash && \
-cp -r endpoints usr/local/emhttp/plugins/undash && \
-cp -r nchan usr/local/emhttp/plugins/undash && \
-chmod -R +x usr/local/emhttp/plugins/undash/nchan/* && \
-tar -cJf releases/undash-$BUILD.txz --owner=0 --group=0 usr/  && \
-rm -rf usr  && \
-cp undash.plg.template undash.plg && \
-sed -i "s/{{BUILD}}/$BUILD/g" undash.plg && \
-sed -i '/<CHANGES>/rCHANGELOG' undash.plg && \
+if (( $# == 1)); then
+ BUILD="$BUILD.$1"
+fi
+echo "BUILDING $BUILD"
+rm -rf usr
+npx vue-cli-service build --dest usr/local/emhttp/plugins/undash
+cp -r endpoints usr/local/emhttp/plugins/undash
+cp -r nchan usr/local/emhttp/plugins/undash
+chmod -R +x usr/local/emhttp/plugins/undash/nchan/*
+tar -cJf releases/undash-$BUILD.txz --owner=0 --group=0 usr/ 
+rm -rf usr 
+cp undash.plg.template undash.plg
+sed -i "s/{{BUILD}}/$BUILD/g" undash.plg
+sed -i '/<CHANGES>/rdocs/CHANGELOG' undash.plg
 echo "BUILD $BUILD COMPLETE"
